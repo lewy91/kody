f077cc001850e6259539da42e0627b57bbaf9cef
lewy91 <heuheu1213@gmail.com>
2018-11-13 14:20:35 +0100

    złoty środek

---
+++ klasa 2/cpp/vibonaczczi.cpp
@@ -0,0 +1,33 @@
+/*
+ * vibonaczczi.cpp
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int fibonacci_it(int n){
+    int a = 0;
+    int b = 1;
+    if (n < 1) return a;
+    else if (n < 2) return b;
+    int wynik= 0;
+    for (int i = 2; i <= n; i++){
+        wynik = a + b;
+        a = b;
+        b = wynik;
+    }
+    return wynik;
+}
+
+int main(int argc, char **argv)
+{
+    int n;
+    cout <<"Podaj numer wyrazu ciągu:";
+    cin >> n;
+	cout <<"Ciąg Fibonacciego do wyrazu" << n << ":" << endl;
+    cout << fibonacci_it(n);
+    
+	return 0;
+}
+
