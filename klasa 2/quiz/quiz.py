ea149466b97eaa759630b0ebb9fc29e003fab814
lewy91 <heuheu1213@gmail.com>
2018-12-14 08:45:17 +0100

    quizy

--- klasa 2/quiz/quiz.py
+++ klasa 2/quiz/quiz.py
@@ -5,19 +5,34 @@
 #  
 
 
-from flask import Flask
-from flask import render_template
+from flask import Flask, g
+from flask import render_template, request
+from modele import *
+
 app = Flask(__name__)
 
 print(__name__)
 
+@app.before_request
+def before_request():
+    g.db = baza
+    g.db.connect()
+    
+@app.after_request
+def after_request(response):
+    g.db.close
+    return response
+    
 @app.route("/")
 def hello():
     return"<hi>Witaj na serwerze!</h1><h2>Aplikacja quiz</h2>"
 
-@app.route("/strona")
-def strona():
+
+@app.route("/quiz")
+def quiz():
+    pytania = Pytanie.select()
     return render_template('strona.html')
+
 
 @app.route("/klasa")
 def klasa():
